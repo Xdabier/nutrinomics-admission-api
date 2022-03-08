e492484569f9fc373b7b708ce7d6f98355a31cce
Badii Boutar <badiiboutar@gmail.com>
2022-03-08 09:34:02 +0100

    origin cors error

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://nutrinomics-admission.herokuapp.com/',
-    ],
+    origin:
+      'http://localhost:4200,https://nutrinomics-admission.herokuapp.com/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
