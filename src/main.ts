ac68eb2bb23b194b6da431efe7b264ef5075450c
Badii Boutar <badiiboutar@gmail.com>
2022-03-08 08:57:44 +0100

    Add prod origin and fixed start command

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://nutrinomics-admission.herokuapp.com/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
